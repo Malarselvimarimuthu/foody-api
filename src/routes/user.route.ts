8054b343ee4dafb9d4c624d54377b480a5eae644
kavin <kavinnishanthan2004@gmail.com>
2024-06-01 20:03:26 +0530

    required api test

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -7,7 +7,7 @@ import userController from '../controllers/user.controller';
 // Defining routers
 const router = Router();
 
-// orders routes
+// user routes
 router.get('/userDetails/:userId', userController.fetchUserDetails);
 
 export default router;
